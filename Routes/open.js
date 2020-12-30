ac71d398eb44ce3a2e9e2a776cc4d8e939794fbc
nikhils4 <nikhilsingh498@gmail.com>
2020-12-30 15:17:20 +0530

    Add /news

--- Routes/open.js
+++ Routes/open.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const axios = require("axios");
 
 router.get( '/', (request, response) => {
     response.status(200).json({
@@ -10,6 +11,20 @@ router.get( '/team', (request, response) => {
     response.status(200).json({
         message : "The API is built by Nikhil, while frontend is developed by Shreya and Saloni"
     })
+})
+
+router.get('/news', (request, response) => {
+  axios.get('http://api.mediastack.com/v1/news?access_key=0fe6be7088aff123737658e7e1ddf9e4&keywords=successful%20women&countries=in&limit=50')
+  .then(function (res) {
+    response.status(200).json({
+      message: res
+    })
+  })
+  .catch(function (error) {
+    response.status(200).json({
+      err: 'There was some error while fetching the results.'
+    })
+  })
 })
 
 module.exports = router;
